66996eea8c1ff464ad2852bb04cb7c97fefa8bab
miguel11065 <126350222+miguel11065@users.noreply.github.com>
2023-03-02 14:43:17 -0300

    segurança adicionada
    adicionada o acesso apenas com a senha

--- cartorio.cpp
+++ cartorio.cpp
@@ -111,49 +111,61 @@ int main()
 {
 	int opcao=0; //definindo variaveis
 	int laco=1;
+	char senhadigitada[10]="a";
+	int comparacao;
 	
-	for(laco=1;laco=10;)
+	printf("\t Cartório da EBAC \n\n");
+	printf("Login do administrador! \n\n Digite a sua senha: ");
+	scanf("%s", senhadigitada);
+	comparacao = strcmp(senhadigitada, "admin");
+	
+	if(comparacao == 0)
 	{
-		system("cls");
 	
-    	setlocale(LC_ALL, "portuguese"); //definindo linguagem
-    	
-		printf("\t Cartório da EBAC \n\n"); //inicio do menu
-		printf("Escolha a opção desejada do menu:\n\n");
-		printf("\t1 - Registrar nomes\n");
-		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Deletar nomes\n"); 
-		printf("\t4 - Sair do sistema\n\n");
-		printf("Opção: "); //fim do menu
-	
-		scanf("%d", &opcao); //armazenando escokha do usuario
-	
-		system("cls"); // responsavel por limpar a tela
-	
-		switch(opcao) // inicio da seleão do menu
+		for(laco=1;laco=10;)
 		{
-			case 1:
-			registro(); // chama a função registro
-    		break;
-    		
-    		case 2:
-    		consulta(); // chama a função consulta
-			break;
-			
-			case 3:
-			deletar(); // chama a função deletar
-			break;
-			
-			case 4:
-			printf("Obrigado por utilizar o sistema!\n");
-			return 0;
-			break;
-			
-			default:
-			printf("Essa opção não está disponivel\n");
-			system("pause");
-			break;
-		}
+			system("cls");
+		
+ 	   		setlocale(LC_ALL, "portuguese"); //definindo linguagem
+    	
+			printf("\t Cartório da EBAC \n\n"); //inicio do menu
+			printf("Escolha a opção desejada do menu:\n\n");
+			printf("\t1 - Registrar nomes\n");
+			printf("\t2 - Consultar nomes\n");
+			printf("\t3 - Deletar nomes\n"); 
+			printf("\t4 - Sair do sistema\n\n");
+			printf("Opção: "); //fim do menu
+		
+			scanf("%d", &opcao); //armazenando escokha do usuario
 	
+			system("cls"); // responsavel por limpar a tela
+	
+			switch(opcao) // inicio da seleão do menu
+			{
+				case 1:
+				registro(); // chama a função registro
+    			break;
+    		
+    			case 2:
+    			consulta(); // chama a função consulta
+				break;
+			
+				case 3:
+				deletar(); // chama a função deletar
+				break;
+				
+				case 4:
+				printf("Obrigado por utilizar o sistema!\n");
+				return 0;
+				break;
+			
+				default:
+				printf("Essa opção não está disponivel\n");
+				system("pause");
+				break;
+			}
+		}
     }
+    else
+    	printf("Senha incorreta!");
 }
